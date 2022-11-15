d809a98748c6a4a1a9d2c59e3155371b78ae821b
unknown <muryllo.henrique@hotmail.com>
2022-11-15 17:50:39 -0300

    finishing the folder reestructure

--- codewars/8 kyu/Math.js
+++ codewars/8 kyu/Math.js
@@ -22,3 +22,12 @@ function findDifference(a, b) {
     );
   }
    console.log(findDifference([3, 2, 5], [1, 4, 4]));
+
+   // - Keep-Hydrated!
+// Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+function litres(time) {
+  return Math.trunc(time * 0.5)
+} console.log(litres(1));
--- codewars/8 kyu/if-else-for.js
+++ codewars/8 kyu/if-else-for.js
@@ -62,3 +62,15 @@ function DNAStrand(dna) {
   return result;
 }
 console.log(DNAStrand("AAAC"));
+
+// - squares-sentece
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+function squares(x, n) {
+  var ret = [];
+  for(var i=0; i<n; i++){
+    ret.push(x);
+    x*= x;
+  }
+  return ret;
+} console.log(squares(2,6));
