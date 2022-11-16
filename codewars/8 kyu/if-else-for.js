9acbb99ba432481c72d7be4c84b3ffcacd8177be
unknown <muryllo.henrique@hotmail.com>
2022-11-16 09:18:58 -0300

    adding First Factorial

--- codewars/8 kyu/if-else-for.js
+++ codewars/8 kyu/if-else-for.js
@@ -74,3 +74,12 @@ function squares(x, n) {
   }
   return ret;
 } console.log(squares(2,6));
+
+// - First Factorial
+
+function FirstFactorial(num) {
+  for (var i = num - 1; i >= 1; i--){
+  num*= i;
+  }
+  return num; 
+} console.log(FirstFactorial(5));
