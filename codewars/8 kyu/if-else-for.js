b50899e411d07e609299e85207da4a60d32c70df
unknown <muryllo.henrique@hotmail.com>
2022-11-25 11:22:01 -0300

    Learning class

--- codewars/8 kyu/if-else-for.js
+++ codewars/8 kyu/if-else-for.js
@@ -112,3 +112,28 @@ function isPythagoreanTriple(integers) {
   return true
   else return false
 } console.log(isPythagoreanTriple(3,4,5));
+
+// - Even-Or-Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+} console.log(even_or_odd(4));
+
+// - ReturnNegative
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+} console.log(makeNegative());
+
+// - Training JS #7: if..else and ternary operator
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.
+// number -	price (cents)
+// n < 5 - 100
+// n >= 5 and n < 10 - 95
+// n >= 10 - 90
+
+function saleHotdogs(n){
+  return n < 5 ? n*100 : n>=5 && n < 10 ? n*95 : n*90
+} console.log(saleHotdogs(6));
